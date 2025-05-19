bbef32a65521a1ac45637f25e6bfebeeb0c14f62
Akshaj6 <akshaj.samaria@gmail.com>
2025-05-19 18:07:31 +0530

    69

---
+++ Frequency_Take_2.py
@@ -0,0 +1,36 @@
+dict = {'Monday':
+    {'skies' : ['Covered With Black Clouds'],
+    'weather' : ['rainy']
+    'temperature' : ['20 degrees']
+    },
+    'Tuesday':
+    {'skies' : ['As Blue As Can Be'],
+    'weather' : ['sunny']
+    'temperature' : ['43 degrees']
+    },
+    'Wednesday':
+    {'skies' : ['Covered With Grey Clouds'],
+    'weather' : ['humid, with chances of rain']
+    'temperature' : ['20 degrees']
+    },
+    'Thursday':
+    {'skies' : ['Covered With Black Clouds'],
+    'weather' : ['rainy']
+    'temperature' : ['20 degrees']
+    },
+    'Friday':
+    {'skies' : ['Covered With Grey Clouds'],
+    'weather' : ['humid, with chances of rain']
+    'temperature' : ['20 degrees']
+    },
+    'Saturday':
+    {'skies' : ['Covered With Grey Clouds'],
+    'weather' : ['humid, with chances of rain']
+    'temperature' : ['20 degrees']
+    },
+    'Sunday':
+    {'skies' : ['As Blue As Can Be'],
+    'weather' : ['sunny']
+    'temperature' : ['43 degrees']
+    },
+}
