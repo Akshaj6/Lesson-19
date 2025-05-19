dc7e003ff6fc5832ed33681c55244d8eea189a39
Akshaj6 <akshaj.samaria@gmail.com>
2025-05-19 18:23:00 +0530

    69

--- Frequency_Take_2.py
+++ Frequency_Take_2.py
@@ -1,36 +1,48 @@
 dict = {'Monday':
     {'skies' : ['Covered With Black Clouds'],
-    'weather' : ['rainy']
+    'weather' : ['rainy'],
     'temperature' : ['20 degrees']
     },
     'Tuesday':
     {'skies' : ['As Blue As Can Be'],
-    'weather' : ['sunny']
+    'weather' : ['sunny'],
     'temperature' : ['43 degrees']
     },
     'Wednesday':
     {'skies' : ['Covered With Grey Clouds'],
-    'weather' : ['humid, with chances of rain']
+    'weather' : ['humid, with chances of rain'],
     'temperature' : ['20 degrees']
     },
     'Thursday':
     {'skies' : ['Covered With Black Clouds'],
-    'weather' : ['rainy']
+    'weather' : ['rainy'],
     'temperature' : ['20 degrees']
     },
     'Friday':
     {'skies' : ['Covered With Grey Clouds'],
-    'weather' : ['humid, with chances of rain']
+    'weather' : ['humid, with chances of rain'],
     'temperature' : ['20 degrees']
     },
     'Saturday':
     {'skies' : ['Covered With Grey Clouds'],
-    'weather' : ['humid, with chances of rain']
+    'weather' : ['humid, with chances of rain'],
     'temperature' : ['20 degrees']
     },
     'Sunday':
     {'skies' : ['As Blue As Can Be'],
-    'weather' : ['sunny']
+    'weather' : ['sunny'],
     'temperature' : ['43 degrees']
     },
 }
+def count_dialogue_frequency(dialogue_dict, target_dialogue):
+    frequency = 0
+    for dialogues in dialogue_dict.values():
+        if isinstance(dialogues, list):
+            for dialogue in dialogues:
+                if target_dialogue.lower() in dialogue.lower():
+                    frequency += 1
+    
+    return frequency
+target_dialogue = input("Enter the dialogue to search for: ")
+frequency = count_dialogue_frequency(target_dialogue)
+print("\nThe dialogue '{target_dialogue}' appears {frequency} times in the dictionary.")
